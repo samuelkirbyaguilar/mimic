e7700dedf96b7c246176e684850ed03c6e1401d0
samuelkirbyaguilar <shaguilar@up.edu.ph>
2022-02-01 02:13:11 +0800

    change root view to use different asset path if in production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,14 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    @if (env("APP_ENV") == "local")
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+    @elseif (env("APP_ENV") == "production")
+        <link href="{{ secure_url("mimic/" . mix('css/app.css')) }}" rel="stylesheet" />
+        <script src="{{ secure_url("mimic/" . mix('js/app.js')) }}" defer></script>
+    @endif
     @inertiaHead
   </head>
   <body>
