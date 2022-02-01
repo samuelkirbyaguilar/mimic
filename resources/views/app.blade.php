76dd766b87a3fc95a3dddb7c11524561db6a9407
samuelkirbyaguilar <shaguilar@up.edu.ph>
2022-02-01 21:30:53 +0800

    change root blade template to use asset() helper

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,14 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
-    @if (env("APP_ENV") == "local")
-        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-        <script src="{{ mix('/js/app.js') }}" defer></script>
-    @elseif (env("APP_ENV") == "production")
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    @endif
+    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet" />
+    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
     @inertiaHead
   </head>
   <body>
